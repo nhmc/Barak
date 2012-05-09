25b7acd9fd6e5b849576eae36dab5376bb416886
Neil Crighton <neilcrighton@gmail.com>
2012-05-09 16:46:54 +0200

    add function to calculate element abundances

--- barak/abundances.py
+++ barak/abundances.py
@@ -23,6 +23,7 @@ cond_temp:
   gaseous state.
 
 """
+import numpy as np
 from utilities import get_data_path
 from io import readtxt
 from collections import OrderedDict
@@ -36,3 +37,38 @@ Asolar = OrderedDict(
 cond_temp = readtxt(datapath +
                     'abundances/CondensationTemperatures.txt',
                     readnames=1, sep='|')
+
+def calc_abund(X, Y, logNX, logNY):
+    """ Find the abundance relative to solar given two elements and
+    their column densities.
+
+    Parameters
+    ----------
+    X, Y : str
+      Element identifiers (for example 'C', 'Si', 'Mg').
+    logNX : array_like, shape (N,)
+      log10 of element X column density in cm^-2.
+    logNY : array_like, shape (N,)
+      log10 of element Y column density in cm^-2.
+
+    Returns
+    -------
+    abundance_ratio : ndarray, shape (N,)
+       Abundance ratio relative to solar, [X/Y].
+
+    Notes
+    -----
+    The abundance ratio is defined::
+
+      [X/Y] = log10 (n_X / n_Y) - log10 (n_Xsun / n_Ysun)
+    
+    Where N_Xsun / N_Ysun is the ratio of the number density of
+    species X to species Y for 'proto-solar' abundances (See Lodders
+    2003, ApJ, 591, 1220). For example, if [X/Y] = 0, it has the same
+    abundance as the proto-solar values. If [X/Y] = 1, then ten times
+    larger, [X/Y] = -1, then ten times smaller.
+
+    If Y is hydrogen, then this estimates the metallicity.
+    """
+    Z = np.log10(10**logNX / 10**logNY) - (Asolar[X] - Asolar[Y])
+    return  Z
