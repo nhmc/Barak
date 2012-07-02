972f822b84b779333462d22dfecfee476700211e
Neil Crighton <neilcrighton@gmail.com>
2012-07-02 11:59:01 +0200

    doc build tweak

--- docs/build_docs.sh
+++ docs/build_docs.sh
@@ -5,6 +5,6 @@ mkdir -p generated
 make html
 # git co gh-pages
 # cd ..; /bin/cp -r docs/_build/html/* .;  git st
-# git add generated _modules _static _sources *html  searchindex.js objects.inv
+# git add generated _modules _static _sources *html _images searchindex.js objects.inv
 # git ci -m'doc build'; git push
 # git co master
