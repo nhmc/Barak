7a411a111c94e8876f467d637fd65d9f17debfab
Neil Crighton <neilcrighton@gmail.com>
2013-01-06 15:37:15 +0100

    update CHANGES to warn of API breakages with respect to version 0.2, add a script outlining all the tasks necessary for a release

---
+++ docs/new_release.sh
@@ -0,0 +1,23 @@
+# change the version number in 
+#
+#    docs/conf.py
+#    setup.py
+#    barak/__init__.py
+#
+# Update CHANGES
+
+# make a new tag
+
+git tag -a v0.3.0
+git push --tags
+
+# build tarball
+python setup.py sdist
+
+# test it installs correctly with
+pip install ./Barak_vx.x.tar.gz
+pip uninstall barak
+
+# Then upload to PyPI
+python setup.py register sdist upload
+
