81cb4068c82330f19d4e4616777b5c2ea72be839
Neil Crighton <neilcrighton@gmail.com>
2013-12-10 10:32:20 +1100

    bug fixing, add more options for generating Voigt profiles

--- barak/absorb.py
+++ barak/absorb.py
@@ -90,7 +90,7 @@ def calc_sigma_on_f(vel, wa0, gam, b, debug=False, verbose=True):
             raise IndexError(4*'%s ' % (len(vel), ic, ic-1, vel))
 
         fwhm = max(gam_v/1.e5, fwhmg/1.e5)
-        
+
         if vstep > fwhm:
             print('Warning: tau profile undersampled!')
             print('  Pixel width: %f km/s, transition fwhm: %f km/s' % (
@@ -105,7 +105,7 @@ def calc_sigma_on_f(vel, wa0, gam, b, debug=False, verbose=True):
     u = 1.e5 / b * vel                         # dimensionless
     a = gam_v / (4*pi*b)                       # dimensionless
     vp = voigt(a, u)                           # dimensionless
-    
+
     # Note the below isn't exactly the same as sigma as defined by
     # Draine et al. It must be multiplied by the oscillator strength
     # and the column density to give the optical depth.
@@ -192,7 +192,7 @@ def calc_tau_peak(logN, b, wa0, osc):
 def logN_from_tau_peak(tau, b, wa0, osc):
     """ Calculate the column density for a transition given its
     width and its optical depth at line centre.
-    
+
     Parameters
     ----------
     tau : array_like
@@ -271,7 +271,7 @@ def calc_iontau(wa, ion, zp1, logN, b, debug=False, ticks=False, maxdv=1000.,
 
     tickmarks = []
     sumtau = np.zeros_like(wa)
-    i0 = i1 = None 
+    i0 = i1 = None
     for i, (wa0, osc, gam) in enumerate(trans):
         tau0 = calc_tau_peak(logN, b, wa0, osc)
         if 1 - exp(-tau0) < 1e-3:
@@ -328,7 +328,7 @@ def find_tau(wa, lines, atomdat=None, per_trans=False, debug=False):
             print('Looks like a VpfitModel.lines-style record array.')
         lines = [(l['name'].replace(' ', ''), l['z'], l['b'], l['logN'])
                  for l in lines]
-        
+
     tau = np.zeros_like(wa)
     if debug:
         print('finding tau...')
@@ -381,8 +381,8 @@ def calc_W(dw, nfl, ner, colo_nsig=2, cohi_nsig=2, redshift=0):
     m_ner = np.median(ner)
     colo_mult = 1 - colo_nsig * m_ner
     cohi_mult = 1 + cohi_nsig * m_ner
-    
-    ew = dw * (1 - nfl) 
+
+    ew = dw * (1 - nfl)
     ewer = dw * ner
     ewhi = dw * (1 - nfl) / colo_mult
     ewhier = dw * ner / colo_mult
@@ -432,7 +432,7 @@ def calc_Wr(i0, i1, wa, tr, ew=None, ewer=None, fl=None, er=None, co=None,
       When calculating one sigma lower error decrease the continuum by
       this fractional amount.  Only used if fl, er and co are also
       given.
-      
+
     Returns
     -------
     A dictionary with keys:
@@ -443,7 +443,7 @@ def calc_Wr(i0, i1, wa, tr, ew=None, ewer=None, fl=None, er=None, co=None,
     Wr        Rest equivalent width in same units as wa
     Wre       1 sigma error on rest equivalent width
     zp1       1 + redshift
-    ngoodpix  number of good pixels contributing to the measurements 
+    ngoodpix  number of good pixels contributing to the measurements
     Nmult     multiplier to get from equivalent width to column density
     saturated Are more than 10% of the pixels between limits saturated?
     ========= =========================================================
@@ -455,12 +455,12 @@ def calc_Wr(i0, i1, wa, tr, ew=None, ewer=None, fl=None, er=None, co=None,
     # transition as saturated
     n_saturated_thresh = int(0.1 * npts)
     saturated = None
-    
+
     if ew is None:
         assert None not in (fl, er, co)
         wedge = find_bin_edges(wa1)
         dw = wedge[1:] - wedge[:-1]
-        ew1 = dw * (1 - fl[i0:i1+1] / co[i0:i1+1]) 
+        ew1 = dw * (1 - fl[i0:i1+1] / co[i0:i1+1])
         ewer1 = dw * er[i0:i1+1] / co[i0:i1+1]
         c = (1 + cohi) * co[i0:i1+1]
         ew1hi = dw * (1 - fl[i0:i1+1] / c)
@@ -472,7 +472,7 @@ def calc_Wr(i0, i1, wa, tr, ew=None, ewer=None, fl=None, er=None, co=None,
             saturated = True
         else:
             saturated = False
-        
+
     else:
         assert None not in (ew, ewer)
         ew1 = np.array(ew[i0:i1+1])
@@ -484,12 +484,12 @@ def calc_Wr(i0, i1, wa, tr, ew=None, ewer=None, fl=None, er=None, co=None,
         return None
     if not good.all():
         ew1[~good] = np.interp(wa1[~good], wa1[good], ew1[good])
-        ewer1[~good] = np.interp(wa1[~good], wa1[good], ewer1[good]) 
+        ewer1[~good] = np.interp(wa1[~good], wa1[good], ewer1[good])
         if fl is not None:
             ew1hi[~good] = np.interp(wa1[~good], wa1[good], ew1hi[good])
-            ewer1hi[~good] = np.interp(wa1[~good], wa1[good], ewer1hi[good]) 
+            ewer1hi[~good] = np.interp(wa1[~good], wa1[good], ewer1hi[good])
             ew1lo[~good] = np.interp(wa1[~good], wa1[good], ew1lo[good])
-            ewer1lo[~good] = np.interp(wa1[~good], wa1[good], ewer1lo[good]) 
+            ewer1lo[~good] = np.interp(wa1[~good], wa1[good], ewer1lo[good])
 
     W = ew1.sum()
     We = sqrt((ewer1**2).sum())
@@ -507,7 +507,7 @@ def calc_Wr(i0, i1, wa, tr, ew=None, ewer=None, fl=None, er=None, co=None,
         Wrehi = Wehi / zp1
         Wrlo = Wlo / zp1
         Wrelo = Welo / zp1
-    
+
     # Assume we are on the linear part of curve of growth (will be an
     # underestimate if saturated). See Draine, "Physics of the
     # Interstellar and Intergalactic medium", ISBN 978-0-691-12214-4,
@@ -555,7 +555,7 @@ def b_to_T(atom, bvals):
 
     # convert everything to cgs
     T = 0.5 * b**2 * amu * mp / kboltz
-    
+
     # b \propto sqrt(2kT/m)
     if len(T) == 1:
         return T[0]
@@ -585,7 +585,7 @@ def T_to_b(atom, T):
 
     T = np.atleast_1d(T)
     b_cms = np.sqrt(2 * kboltz * T / (mp *amu))
-    
+
     b_kms = b_cms / 1e5
 
     # b \propto sqrt(2kT/m)
@@ -631,7 +631,7 @@ def readatom(filename=None, debug=False,
       If True, also return data for H2 and CO molecules.
     isotopes : bool (False)
       If True, also return data for isotopes.
-      
+
     Returns
     -------
     atom [, atom_flat] : dict [, dict]
@@ -691,7 +691,7 @@ def readatom(filename=None, debug=False,
 
     if flat:
         return atom, atomflat
-    else: 
+    else:
         return atom
 
 def findtrans(name, atomdat=None):
@@ -710,16 +710,20 @@ def findtrans(name, atomdat=None):
     elif name[:3] == 'HI2':
         i = 3
     else:
-        while name[i].isalpha() or name[i] == '*': i += 1
+        while i < len(name) and (name[i].isalpha() or name[i] == '*'):
+            i += 1
     ion = name[:i]
-    try:
-        wa = float(name[i:])
-    except:
-        print(name, repr(name[:i]), repr(name[i:]))
-        raise
     # must be sorted lowest to highest for indexnear
     isort = np.argsort(atomdat[ion].wa)
     sortwa = atomdat[ion].wa[isort]
+
+    try:
+        wa = float(name[i:])
+    except:
+        print('Possible transitions for', ion)
+        print(sortwa)
+        return
+
     ind = indexnear(sortwa, wa)
     tr = atomdat[ion][isort[ind]]
     # Make a short string that describes the transition, like 'CIV 1550'
@@ -794,11 +798,12 @@ def tau_LL(logN, wa, wstart=912):
     sigma0 = 6.304e-18           # cm^2
     i = wa.searchsorted(wstart)
     tau = np.zeros_like(wa)
-    tau[:i] = 10**logN * sigma0 * (wa[:i] / 912.)**3 
+    tau[:i] = 10**logN * sigma0 * (wa[:i] / 912.)**3
     return tau
 
 def calc_DLA_tau(wa, z=0, logN=20.3, logZ=0, bHI=50, atom=None,
-                 verbose=1, highions=1, molecules=False):
+                 verbose=1, highions=1, molecules=False, same_b=False,
+                 metals=True):
     """ Create the optical depth due to absorption from a DLA.
 
     The DLA is at z=0. The column density and metallicity can be
@@ -814,6 +819,8 @@ def calc_DLA_tau(wa, z=0, logN=20.3, logZ=0, bHI=50, atom=None,
        (the default) gives solar abundances, -1 gives 1/10th of solar.
     bHI : float (50)
        The b parameter to use for HI.
+    same_b : True
+       Use the same b parameter for all species.
     molecules : bool (False)
        Whether to include absorption from H2 and CO.
     verbose : bool (False)
@@ -829,43 +836,54 @@ def calc_DLA_tau(wa, z=0, logN=20.3, logZ=0, bHI=50, atom=None,
     if atom is None:
         atom = _get_atomdat()
 
-    off = -12 + logN + logZ
-    temp = b_to_T('H', bHI)
-    print('b %.2f km/s gives a temperature of %.1f K' % (bHI, temp))
-    
-    elements = 'O Si Fe C Ca Al Ti N Zn Cr'.split()
-    b = dict((el, T_to_b(el, temp)) for el in elements)
-    print('using low ion b values:')
-    print(', '.join('%s %.1f' % (el, b[el]) for el in elements))
-
     f26 = [
         'HI    %.6f  0  %.2f 0 %.2f 0' % (z, bHI, logN),
-        'OI    %.6f  0  %.2f 0 %.2f 0' % (z, b['O'], (Asolar['O']  + off)), 
-        'SiII  %.6f  0  %.2f 0 %.2f 0' % (z, b['Si'], (Asolar['Si'] + off - 0.05)), 
-        'SiIII %.6f  0  %.2f 0 %.2f 0' % (z, b['Si'], (Asolar['Si'] + off - 1)),    
-        'FeII  %.6f  0  %.2f 0 %.2f 0' % (z, b['Fe'], (Asolar['Fe'] + off)),    
-        'CII   %.6f  0  %.2f 0 %.2f 0' % (z, b['C'], (Asolar['C']  + off - 0.05)),    
-        'CIII  %.6f  0  %.2f 0 %.2f 0' % (z, b['C'], (Asolar['C']  + off - 1)),    
-        'CaII  %.6f  0  %.2f 0 %.2f 0' % (z, b['Ca'], (Asolar['Ca'] + off)),
-        'AlII  %.6f  0  %.2f 0 %.2f 0' % (z, b['Al'], (Asolar['Al'] + off - 0.05)),
-        'AlIII %.6f  0  %.2f 0 %.2f 0' % (z, b['Al'], (Asolar['Al'] + off - 1)),
-        'TiII  %.6f  0  %.2f 0 %.2f 0' % (z, b['Ti'], (Asolar['Ti'] + off)),
-        'NII   %.6f  0  %.2f 0 %.2f 0' % (z, b['N'], (Asolar['N']  + off)),
-        'ZnII  %.6f  0  %.2f 0 %.2f 0' % (z, b['Zn'], (Asolar['Zn']  + off)),
-        'CrII  %.6f  0  %.2f 0 %.2f 0' % (z, b['Cr'], (Asolar['Cr']  + off)),
         ]
-    if highions:
-        print('Including O VI, Si IV, C IV and N V')
-        logNCIV = 15.
+
+    if metals:
+        off = -12 + logN + logZ
+        temp = b_to_T('H', bHI)
+        if verbose:
+            print('b %.2f km/s gives a temperature of %.1f K' % (bHI, temp))
+
+        elements = 'O Si Fe C Ca Al Ti N Zn Cr'.split()
+        if same_b:
+            b = dict((el, bHI) for el in elements)
+        else:
+            b = dict((el, T_to_b(el, temp)) for el in elements)
+        if verbose:
+            print('using low ion b values:')
+            print(', '.join('%s %.1f' % (el, b[el]) for el in elements))
+
+        f26 = f26 + [
+            'OI    %.6f  0  %.2f 0 %.2f 0' % (z, b['O'], (Asolar['O']  + off)),
+            'SiII  %.6f  0  %.2f 0 %.2f 0' % (z, b['Si'], (Asolar['Si'] + off - 0.05)),
+            'SiIII %.6f  0  %.2f 0 %.2f 0' % (z, b['Si'], (Asolar['Si'] + off - 1)),
+            'FeII  %.6f  0  %.2f 0 %.2f 0' % (z, b['Fe'], (Asolar['Fe'] + off)),
+            'CII   %.6f  0  %.2f 0 %.2f 0' % (z, b['C'], (Asolar['C']  + off - 0.05)),
+            'CIII  %.6f  0  %.2f 0 %.2f 0' % (z, b['C'], (Asolar['C']  + off - 1)),
+            'CaII  %.6f  0  %.2f 0 %.2f 0' % (z, b['Ca'], (Asolar['Ca'] + off)),
+            'AlII  %.6f  0  %.2f 0 %.2f 0' % (z, b['Al'], (Asolar['Al'] + off - 0.05)),
+            'AlIII %.6f  0  %.2f 0 %.2f 0' % (z, b['Al'], (Asolar['Al'] + off - 1)),
+            'TiII  %.6f  0  %.2f 0 %.2f 0' % (z, b['Ti'], (Asolar['Ti'] + off)),
+            'NII   %.6f  0  %.2f 0 %.2f 0' % (z, b['N'], (Asolar['N']  + off)),
+            'ZnII  %.6f  0  %.2f 0 %.2f 0' % (z, b['Zn'], (Asolar['Zn']  + off)),
+            'CrII  %.6f  0  %.2f 0 %.2f 0' % (z, b['Cr'], (Asolar['Cr']  + off)),
+            ]
+    if highions and metals:
+        if verbose:
+            print('Including O VI, Si IV, C IV and N V')
+        logNCIV = 14.
         logNSiIV = logNCIV - (Asolar['C'] - Asolar['Si'] )
         f26 = f26 + [
-            'OVI   %.6f  0  30.0 0 15.0 0' % z,
+            'OVI   %.6f  0  30.0 0 14.5 0' % z,
             'SiIV  %.6f  0  %.2f  0 %.2f 0' % (z, b['Si'], logNSiIV),
             'CIV   %.6f  0  %.2f 0 %.2f 0' % (z, b['C'], logNCIV),
-            'NV    %.6f  0  30.0 0 15.0 0' % z,
+            'NV    %.6f  0  30.0 0 14.0 0' % z,
             ]
     if molecules:
-        print('Including H_2 and CO')
+        if verbose:
+            print('Including H_2 and CO')
         f26 = f26 + [
             'H2J0  %.6f  0  5.0  0 18.0 0' % z,
             'H2J1  %.6f  0  5.0  0 19.0 0' % z,
@@ -874,11 +892,12 @@ def calc_DLA_tau(wa, z=0, logN=20.3, logZ=0, bHI=50, atom=None,
 
     tau, ticks = find_tau(wa, StringIO('\n'.join(f26)), atomdat=atom)
     ticks.sort(order=str('wa'))
-    
+
     return tau, ticks
 
 def calc_DLA_trans(wa, redshift, vfwhm, logN=20.3, logZ=0, bHI=50,
-                   highions=True, molecules=False):
+                   highions=True, molecules=False, same_b=False,
+                   verbose=True, metals=False):
     """ Find the transmission after absorption by a DLA
 
     Parameters
@@ -903,11 +922,13 @@ def calc_DLA_trans(wa, redshift, vfwhm, logN=20.3, logZ=0, bHI=50,
     ticks : ndarray, shape (M,)
       Information for making ticks to show absorbing components.
     """
-    wa1 = make_constant_dv_wa_scale(wa[0], wa[-1], vfwhm/3.)  
+    npix = max(3, int(vfwhm/30.))
+    wa1 = make_constant_dv_wa_scale(wa[0], wa[-1], vfwhm/npix)
     tau, ticks = calc_DLA_tau(
         wa1, z=redshift, logN=logN, logZ=logZ, bHI=bHI,
-        highions=highions, molecules=molecules)
-    trans = convolve_psf(np.exp(-tau),  3.)
+        highions=highions, molecules=molecules, same_b=same_b,
+        verbose=verbose, metals=metals)
+    trans = convolve_psf(np.exp(-tau),  npix)
     trans1 = np.interp(wa, wa1, trans)
     return trans1, ticks
 
@@ -926,7 +947,7 @@ def guess_logN_b(ion, wa0, osc, tau0):
         T = 30000
     elif ion in ('CIV', 'SiIV'):
         T = 20000
-        
+
     b = T_to_b(split_trans_name(ion)[0], T)
 
     if ion == 'OVI':
@@ -940,7 +961,7 @@ def Nvel_from_tau(tau, wa, osc):
     Parameters
     ----------
     tau : array_like, shape (N,)
-      Array of optical depths/    
+      Array of optical depths/
     wa : float
       Rest wavelength of the transition.
     osc : float
@@ -953,7 +974,7 @@ def Nvel_from_tau(tau, wa, osc):
       (km/s)^-1 Multiply this by a velocity interval in km/s to get a
       column density.
     """
-    # the 1e5 here converts from (cm/s)^-1 to (km/s)^-1 
+    # the 1e5 here converts from (cm/s)^-1 to (km/s)^-1
     return tau / (pi * e2_me_c * osc) / (wa * 1e-8) * 1e5
 
 
@@ -964,7 +985,7 @@ def Nlam_from_tau(tau, wa, osc):
     Parameters
     ----------
     tau : array_like, shape (N,)
-      Array of optical depths/    
+      Array of optical depths/
     wa : float
       Rest wavelength of the transition.
     osc : float
@@ -978,7 +999,7 @@ def Nlam_from_tau(tau, wa, osc):
       Angstroms corresponding to the width of one input pixel to get a
       column density.
     """
-    # the 1e-8 here converts from cm^-1 to Angstrom^-1 
+    # the 1e-8 here converts from cm^-1 to Angstrom^-1
     return tau / (pi * e2_me_c * osc) * c / (wa * 1e-8)**2 * 1e-8
 
 def log10N_from_Wr(Wr, wa0, osc):
@@ -1010,7 +1031,7 @@ def log10N_from_Wr(Wr, wa0, osc):
     if not Wr > 0:
         return 0
 
-    Nmult = 1.13e20 / (osc * wa0**2)    
+    Nmult = 1.13e20 / (osc * wa0**2)
     return np.log10(Nmult * Wr)
 
 
@@ -1040,7 +1061,7 @@ def tau_from_nfl_ner(nfl, ner, sf=1):
     factor is typically 1, it can be non zero if you want to scale the
     optical depth of one transition to another (e.g. to infer CIV 1550
     from CIV 1558).
-    """ 
+    """
     # fast path to avoid expensive checks for array inputs
     try:
         float(nfl)
@@ -1059,7 +1080,7 @@ def tau_from_nfl_ner(nfl, ner, sf=1):
         c2 = ~(c0 | c1)
         tau[c2] = -np.log(nfl[c2]) * sf[c2]
         return tau
-        
+
     if nfl >= 1:
         return 0
     elif nfl < ner:
@@ -1095,17 +1116,17 @@ def tau_cont_mult(nfl, ner, colo_mult, cohi_mult,
 
     Returns
     -------
-    taulo, tau, tauhi, nfl_min, nfl_max : 
+    taulo, tau, tauhi, nfl_min, nfl_max :
       Minimum, best and maximum optical depths, and the flux minimum
       and maximum based on the input continuum scale factors.
     """
     # highest flux from continuum, zero level, and 1 sigma variation
-    zoff = zerolo_nsig * ner 
+    zoff = zerolo_nsig * ner
     nfl_max = max(nfl / colo_mult, nfl + ner, (nfl + zoff) / (1 + zoff))
     taulo = tau_from_nfl_ner(nfl_max, ner / colo_mult, sf=sf)
     # lowest flux from continuum, zero_level, and 1 sigma variation, but no lower
     # than ner
-    zoff = zerohi_nsig * ner 
+    zoff = zerohi_nsig * ner
     nfl_min = max(
         min(nfl / cohi_mult, nfl - ner, (nfl - zoff) / (1 - zoff)),
         ner)
@@ -1130,7 +1151,7 @@ def tau_cont_sigmult(nfl, ner, colo_nsig, cohi_nsig,
 
     Returns
     -------
-    taulo, tau, tauhi, nfl_min, nfl_max : 
+    taulo, tau, tauhi, nfl_min, nfl_max :
       Minimum, best and maximum optical depths, and the flux minimum
       and maximum based on the input continuum scale factors.
 
@@ -1164,7 +1185,7 @@ def calc_N_AOD(wa, nfl, ner, colo_sig, cohi_sig, zerolo_nsig, zerohi_nsig,
             taulo.append(np.nan)
             tau.append(np.nan)
             tauhi.append(np.nan)
-           
+
         tlo, t, thi, f0, f1 = tau_cont_sigmult(
             nfl[i], ner[i], colo_sig, cohi_sig, zerolo_nsig, zerohi_nsig)
         if f0 <= ner[i]:
@@ -1180,7 +1201,7 @@ def calc_N_AOD(wa, nfl, ner, colo_sig, cohi_sig, zerolo_nsig, zerohi_nsig,
     for t in taulo, tau, tauhi:
         # interpolate across any bad values
         t = np.array(t)
-        c0 = np.isnan(t) 
+        c0 = np.isnan(t)
         if c0.any():
             x = np.arange(len(t))
             t[c0] = np.interp(x[c0], x[~c0], t[~c0])
@@ -1191,7 +1212,7 @@ def calc_N_AOD(wa, nfl, ner, colo_sig, cohi_sig, zerolo_nsig, zerohi_nsig,
 
     return logNlo, logN, logNhi, saturated
 
-def calc_N_trans(wa, fl, er, co, trans, redshift, vmin, vmax, 
+def calc_N_trans(wa, fl, er, co, trans, redshift, vmin, vmax,
                  colo_nsig=2, cohi_nsig=2, zerolo_nsig=2, zerohi_nsig=2,
                  atomdat=None):
     """ Measure N for a series of transitions over the given velocity
@@ -1284,7 +1305,7 @@ def calc_N_trans(wa, fl, er, co, trans, redshift, vmin, vmax,
             # upper limit
             s = '$< %.3f$' % max(logNhi_W, logNlim5sig)
         else:
-            hi_er = logNhi - logN 
+            hi_er = logNhi - logN
             lo_er = logN - logNlo
             s = '$%.3f^{%+.3f}_{%+.3f}$' % (logN, hi_er, -lo_er)
 
@@ -1340,5 +1361,3 @@ def get_ionization_energy(species):
 
 #     else:
 #         raise ValueError('Unknown xidl linelist %s' % name)
-
-    
