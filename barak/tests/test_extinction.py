16e4f265b92fb5a2beba80ebb1f78ac7a94d6b85
Neil Crighton <neilcrighton@gmail.com>
2015-04-15 10:46:17 +1000

    add test

--- barak/tests/test_extinction.py
+++ barak/tests/test_extinction.py
@@ -5,6 +5,8 @@ def basic_tests():
     wa = np.arange(900, 22000, 5)
     MW_Cardelli89(wa, EBmV=0.05)
     starburst_Calzetti00(wa, EBmV=0.05)
+    # test regime in which interpolation from MW to Calzetti occurs
+    starburst_Calzetti00([3000], EBmV=0.05)
     LMC_Gordon03(wa, EBmV=0.05)
     LMC2_Gordon03(wa, EBmV=0.05)
     SMC_Gordon03(wa, EBmV=0.05)
